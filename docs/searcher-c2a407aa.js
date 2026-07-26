b963c93ba7f634b89d1b87a0ec9a2876aa0ee0fe
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2026-07-26 17:41:32 +0900

    ベクトル,行列の微分演算について追加します.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19d52eba.js',
+                path_to_root + 'searchindex-b3571e9f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
